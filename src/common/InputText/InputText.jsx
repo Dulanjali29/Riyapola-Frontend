1a47b592b4bb46151e53f48d9c9d4a29530da742
Dulanjali29 <dulanjiariyarathna@gmail.com>
2024-03-14 13:07:49 +0530

    create Input Text component

---
+++ src/common/InputText/InputText.jsx
@@ -0,0 +1,8 @@
+import * as React from 'react';
+import TextField from '@mui/material/TextField';
+
+export default function inputText({label,value,onChange,width}){
+return(
+    <TextField id="outlined-basic" label={label} variant="outlined"  value={value } onChange={onchange} sx={{width,}} />
+)
+}
--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -7,7 +7,14 @@ import MyButton from '../../common/Button/MyButton'
 export default function Login() {
   return (
     <Box>
-    <MyButton/>
+      <Box>
+        <InputText />
+        <InputText />
+
+      </Box>
+      <Box>
+        <MyButton name={"Login"} width={'50%'} background={"#000080"} hoverColor={"#008080"}/>
+      </Box>
     </Box>
   )
 }
